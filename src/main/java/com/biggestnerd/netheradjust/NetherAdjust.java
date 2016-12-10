2065f63ad79d75df1f61c99ebc98a834570fa4a6
biggestnerd <TealNerd@users.noreply.github.com>
2016-12-10 02:32:27 -0600

    update

--- src/main/java/com/biggestnerd/netheradjust/NetherAdjust.java
+++ src/main/java/com/biggestnerd/netheradjust/NetherAdjust.java
@@ -1,41 +1,45 @@
 package com.biggestnerd.netheradjust;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World.Environment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerPortalEvent;
-import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NetherAdjust extends JavaPlugin implements Listener {
 	
-	private double toOverworld;
-	private double toNether;
+	private int overworldRatio;
+	private int netherRatio;
 
 	public void onEnable() {
 		saveDefaultConfig();
 		reloadConfig();
-		int overworldRatio = getConfig().getInt("overworld_ratio", 8);
-		int netherRatio = getConfig().getInt("nether_ratio", 1);
-		toOverworld = overworldRatio / (netherRatio * 8);
-		toNether = 8 * netherRatio / overworldRatio;
+		overworldRatio = getConfig().getInt("overworld_ratio", 4);
+		netherRatio = getConfig().getInt("nether_ratio", 1);
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	@EventHandler
 	public void onPlayerPortal(PlayerPortalEvent event) {
-		if(event.getCause() != TeleportCause.NETHER_PORTAL) return;
-		Location to = event.getTo();
-		Environment env = to.getWorld().getEnvironment();
-		double newX, newZ;
-		if(env == Environment.NETHER) {
-			newX = toNether * to.getBlockX();
-			newZ = toNether * to.getBlockZ();
+		if(event.isCancelled()) return;
+		
+		Location from = event.getFrom().clone();
+		if(from.getBlock().getType() != Material.PORTAL) return;
+		Location to = event.getTo().clone();
+		event.useTravelAgent(true);
+		
+		double scale;
+		double yscale = 1d * from.getWorld().getMaxHeight() / to.getWorld().getMaxHeight();
+		if(from.getWorld().getEnvironment() == Environment.NETHER) {
+			scale = netherRatio / overworldRatio;
 		} else {
-			newX = toOverworld * to.getBlockX();
-			newZ = toOverworld * to.getBlockZ();
+			scale = overworldRatio / netherRatio;
 		}
-		event.setTo(new Location(to.getWorld(), newX, to.getY(), newZ));
+		Location newTo = new Location(to.getWorld(), from.getX() * scale, from.getY() * yscale, from.getZ() * scale);
+		event.setTo(newTo);
+		event.getPortalTravelAgent().setCanCreatePortal(true);
+		event.setTo(event.getPortalTravelAgent().findOrCreate(event.getTo()));
 	}
 }
